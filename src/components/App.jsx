9f330894f71fec3d64af31a1b6559eacea257a3b
Dev Pipalia <pipaliadev@gmail.com>
2023-02-14 23:57:01 +0530

    Added Navbar

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { ReactDOM } from "react";
+import Navbar from "./Navbar";
+import Hero from "./Hero";
+
+function App(){
+    return(
+    <div>
+    <Navbar/>
+    </div>
+    )
+}
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { ReactDOM } from "react";
+
+function Navbar(){
+    return(
+    <div className="nav-bar">
+    <img className="logo" src="https://bit.ly/Airbnb_Logo"></img>
+    <h2 className="logo-name">airbnb</h2>
+    </div>
+    )
+}
+
+export default Navbar;
