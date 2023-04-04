9cbe832a5cfa4c74217e573d7699d636cdff2dac
Dev Pipalia <devpipalia003@gmail.com>
2023-04-04 21:59:44 +0530

    sample

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,15 +1,19 @@
 import React from "react";
 
 function Card(props){
+    
+
     return(
         
         <div className="cardd">
         <div className="card-container">
-        
+            <div  className="card-badge"> ❤</div>
             <img className="card-img" src={props.image}></img>
             <p className="card-content rating">{props.rating}</p>
             <p className="card-content info">{props.info}</p>
             <p className="card-conten price"><b className="pricee">{props.pricing} </b>/ <em>per person</em></p>
+        
+        
         </div>
         </div>
     )
